edc0be7c2a04d4c58b46afe19f09541b2aa270db
xulh <xulh@yidianwan.cn>
2021-02-19 11:42:08 +0800

    update

--- native-nio/src/test/java/Test.java
+++ native-nio/src/test/java/Test.java
@@ -1,5 +1,3 @@
-package com.op;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
--- netty-server/src/main/java/com/test/NettyConfig.java
+++
@@ -1,6 +0,0 @@
-package com.test;
-
-public class NettyConfig {
-	public static String HOST = "127.0.0.1";
-	public static int PORT = 9000;
-}
