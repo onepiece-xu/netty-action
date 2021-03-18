206fbb56c3d811ef359f69a0465dfae766fd5b4b
xulh <779682356@qq.com>
2021-03-18 20:34:45 +0800

    update

--- ydw-protocol/src/main/java/com/op/MyClientHandler.java
+++ ydw-protocol/src/main/java/com/op/MyClientHandler.java
@@ -1,10 +1,12 @@
 package com.op;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.nio.charset.Charset;
+import java.util.Random;
 
 /**
  * @author xulh
@@ -13,11 +15,36 @@ import java.nio.charset.Charset;
  */
 public class MyClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
+    int a = 0;
+
+    MyClientHandler(){
+        System.out.println("MyClientHandler init");
+    }
+
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println(++a);
         int i = msg.readableBytes();
         byte[] bytes = new byte[i];
         msg.readBytes(bytes);
         System.out.println(ctx.channel().remoteAddress()+ " "+new String(bytes, "GBK"));
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//        System.out.println(ctx.channel().remoteAddress()+ " channelActive");
+        ByteBuf byteBuf = Unpooled.buffer();
+        byteBuf.writeShort(2);
+        byteBuf.writeShort(1);
+        byteBuf.writeInt(0x0105);
+        byteBuf.writeInt(new Random().nextInt());
+        Long l = System.currentTimeMillis() / 1000;
+        byteBuf.writeInt(l.intValue());
+        StringBuilder sb = new StringBuilder();
+        sb.append("deviceid:"+a);
+        byte[] s = sb.toString().getBytes();
+        byteBuf.writeInt(s.length);
+        byteBuf.writeBytes(s);
+        ctx.writeAndFlush(byteBuf);
     }
 
 }
